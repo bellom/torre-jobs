7c76377130d66c375fa23521c93ee498cb3a6789
oluwaseun bello <bellomsean@gmail.com>
2021-07-01 11:37:21 +0100

    Added homepage design

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -24,55 +24,49 @@ const HomePage = () => {
     fetchJobs()
   }, [fetchUser, fetchJobs])
 
-  console.log({user})
-  console.log({jobs})
-  // For user details::::::::
-  // Profile Picture - res.person.picture
-  // Headline - res.person.professionalHeadline
-  // Name - res.person.name
-  // country - res.person.location.country
-
-  // For job list:::::::::
-  // Job title - jobs.objective
-  // Job type - jobs.type
-  // company name- job.organizations.name
-
+  const image = user?.person?.picture
+  const headline = user?.person?.professionalHeadline
+  const name = user?.person?.name
+  const country = user?.person?.location?.country
 
   return (
-    // <div className="homepage">
-    //   {/* <Header /> */}
-    //   <div className="user-grid">
-    //     <h2>User Details</h2>
-    //       {JSON.stringify(user, null, 2)}
-    //   </div>
-    //   <div>
-    //     <h2>List of jobs relevant to your profile</h2>
-    //     <ul>
-    //       {jobs.map(e => (
-    //         <li key={e.id}>
-    //           <Link to={`/job/${e.id}`}>
-    //             {JSON.stringify(e, null, 2)}
-    //           </Link>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    // </div>
-
-    <div class="container">
-      <div class="user-cards">
-      <h2>User Details</h2>
-        <div class="img-wrapper">
-          <img src="https://maddieshome.files.wordpress.com/2018/11/wall-art-2852231_960_720.jpg" alt="" class='image' />
-        </div>
-        <div class='user-info'>
-          <div class="headline">Headline</div>
-          <div className="name">Name</div>
-          <div className="country">Country</div>
-        </div>
+    <div className="container">
+      <div className="titles">
+        <span>Profile</span>
+        <span className="span">List of jobs relevant to your profile</span>
       </div>
+      <div className="cards">
+        <div className="user-cards">
+          <div className="img-wrapper">
+            <img src={image} alt="" class='image' />
+          </div>
+          <div className='user-info'>
+            <div className="headline">{headline}</div>
+            <div className="name">{name}</div>
+            <div className="country">{country}</div>
+          </div>
+        </div>
 
-      <div class="job-cards">
+        <div className="job-cards">
+          {jobs.map(e => (
+              <div key={e.id} className="job-card">
+                <Link to={`/job/${e.id}`}>
+                  <div>
+                    <span>Job Title:</span>
+                    <span>{e.objective}</span>
+                  </div>
+                  <div>
+                    <span>Job Type:</span>
+                    <span>{e.type}</span>
+                  </div>
+                  <div>
+                    <span>Company Name:</span>
+                    <span>{e.organizations[0].name}</span>
+                  </div>
+                </Link>
+              </div>
+          ))}
+        </div>
       </div>
     </div>
   )
